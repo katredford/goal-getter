493db2edbcc8ed319af5f63e5007597655df473b
katredford <katharine.redford@gmail.com>
2023-11-17 00:06:10 -0600

    week and month are fixed, something up with day still

--- src/components/goalCard/index.jsx
+++ src/components/goalCard/index.jsx
@@ -12,15 +12,19 @@ export default function GoalCard({ task }) {
 
   useEffect(() => {
     const resetClickCount = () => {
-     let now = new Date();
+      let now = new Date();
+      now = startOfDay(addDays(now, 1));
+      console.log("midnight",now)
+      // now = addDays(startOfDay(now), 31 - now.getDate());
+      // console.log("NEXT MONTH", now)
       if (task.timePeriod === 'day') {
         setClickNum(0);
         setProgress(0)
 
       // Calculate the time until the next midnight
-        const now = new Date();
-        // const now = new Date('2023-11-21T00:00:00.000Z');
-        console.log(now)
+        // const now = new Date();
+        // const now = new Date('2023-11-18T00:00:00.000Z');
+        // console.log(now)
       const midnight = startOfDay(addDays(now, 1));
 
       // Calculate the delay until the next midnight
@@ -30,35 +34,61 @@ export default function GoalCard({ task }) {
         setTimeout(resetClickCount, delay);
       }
 
-      if (task.timePeriod === 'week') {
+      // if (task.timePeriod === 'week') {
+
+      //   // const now = new Date();
+      //   let now = new Date('2023-11-20T00:00:00.000Z');
+
+
+
+      //   if (getDay(now) === 1) {
+      //     setClickNum(0);
+      //     setProgress(0);
+      //     const updatedTask = { ...task, clickNum: 0, completeCirc: 0 };
+      //     localStorage.setItem(`task-${task.name}`, JSON.stringify(updatedTask));
+      //     // Calculate the time until the next Monday midnight
+      //     const nextMonday = addDays(startOfDay(now), (8 - getDay(now)) % 7);
+      //     const delay = nextMonday - now;
+
+      //     // Set a timeout for the next Monday midnight
+      //     setTimeout(resetClickCount, delay);
+      //   }
+      // }
+      //  now = new Date('2023-11-21T00:00:00.000Z');
+
+      // now = addDays(startOfDay(now), (8 - getDay(now)) % 7);
+     
+      if (task.timePeriod === 'week' && getDay(now) === 1) {
        
-        const now = new Date();
-        // const now = new Date('2023-11-21T00:00:00.000Z');
-        
-        
-        if (getDay(now) === 1) {
-          setClickNum(0);
-          setProgress(0);
+        setClickNum(0);
+        setProgress(0);
+        const updatedTask = { ...task, clickNum: 0, completeCirc: 0 };
+        localStorage.setItem(`task-${task.name}`, JSON.stringify(updatedTask));
 
-          // Calculate the time until the next Monday midnight
-          const nextMonday = addDays(startOfDay(now), (8 - getDay(now)) % 7);
-          const delay = nextMonday - now;
+        // Calculate the time until the next Monday midnight
+        const nextMonday = addDays(startOfDay(now), (8 - getDay(now)) % 7);
+        console.log("next monday",nextMonday)
+        // If it's already Monday, set the delay to midnight; otherwise, set the delay to the next Monday
+        const delay = getDay(now) === 1 ? nextMonday - now : 24 * 60 * 60 * 1000;
 
-          // Set a timeout for the next Monday midnight
-          setTimeout(resetClickCount, delay);
-        }
+        // Set a timeout for the next Monday midnight
+        setTimeout(resetClickCount, delay);
       }
+    
 
+     
       if (task.timePeriod === "month") {
 
-        now = new Date('2023-12-01T00:00:00.000Z');
+        // now = new Date('2023-12-01T00:00:00.000Z');
         console.log("NONONOW", now)
         // if (getDay(now) === 1 && now.getDate() === 1) {
+        const updatedTask = { ...task, clickNum: 0, completeCirc: 0 };
+        localStorage.setItem(`task-${task.name}`, JSON.stringify(updatedTask));
           setClickNum(0);
           setProgress(0);
 
           // Calculate the time until the next month's first day
-          const nextMonth = addDays(startOfDay(now), 32 - now.getDate());
+          const nextMonth = addDays(startOfDay(now), 31 - now.getDate());
           const delay = nextMonth - now;
 
           // Set a timeout for the next month's first day
@@ -74,6 +104,7 @@ export default function GoalCard({ task }) {
     return () => clearTimeout();
   }, [task.name, task.timePeriod]);
  
+
 
   useEffect(() => {
     const storedTask = localStorage.getItem(`task-${task.name}`);
