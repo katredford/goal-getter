7cd6234fc625d518bdc996c44fd76a18b010e0f3
katredford <katharine.redford@gmail.com>
2023-12-05 18:38:31 -0600

    remove vitePWA

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      workbox: {
-        clientsClaim: true,
-        skipWaiting: true
-      }
-    })
+    
   ],
 })
